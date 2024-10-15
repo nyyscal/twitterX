30f740ad13b950b40db606d1e9459e9f6cfa1922
Nischal <nyyscal541556@gmail.com>
2024-10-15 22:07:22 +0545

    follow and unfollow by customHook

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -52,7 +52,7 @@ export const followUnfollowUser = async(req,res) =>{
       })
       
       await newNotification.save();
-      //TODO return the id of user as a response
+      
       res.status(200).json({message:"User followed succesfully!"})
 
     }
